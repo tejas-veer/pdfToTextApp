9c9097f19cf2a6820747a2af4ea82f0b0709ff5a
Pcode13 <veer.pragati99@gmail.com>
2022-12-23 18:15:13 +0530

    base,login,signup template setup

--- pdfapp/views.py
+++ pdfapp/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+
+def login(request):
+    return render(request,"login.html")
+
+
+def signup(request):
+    return render(request,"signup.html")
