1d67746409e654ea95026251e77f894766a3061a
Pcode13 <veer.pragati99@gmail.com>
2022-12-26 13:03:07 +0530

    format

--- pdfapp/views.py
+++ pdfapp/views.py
@@ -3,22 +3,60 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import login as auth_login
-import cloudinary.uploader 
 from .models import TextModel
 from django.http import *
 from django.contrib import messages
-import requests
 
 import io
-from PIL import Image
 import pytesseract
-from wand.image import Image as wi
+from PIL import Image
+import cloudinary.uploader 
 from pdf2image import convert_from_bytes
 
-# Create your views here.
-pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe" 
+
+def signup(request):
+    if request.method == "POST":
+        data = request.POST
+        name = data['name']
+        username = data['username']
+        password = data['password']
+        confirm_password = data['confirm_password']
+
+        user = User.objects.filter(username=username).exists()
+        if not user :
+            if password == confirm_password:
+                user = User.objects.create_user(username=username,password=password,first_name=name)
+                return redirect("login")
+            else:
+                messages.error(request,"Password does not match")
+                return redirect('signup')
+        else:
+            messages.error(request, "Username already exist")
+            return redirect('signup')
+
+    return render(request,"signup.html")
+
+def login(request):
+    if request.method == "POST":
+        data = request.POST
+        username = data['username']
+        password = data['password']
+
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            auth_login(request,user)
+            return redirect("home")
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('login')
+    return render(request,"login.html")
+
+def logout(request):
+    auth_logout(request)
+    return redirect("login")
 
 def ocr(target_file):
+    pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe" 
     request_file = target_file.file.getvalue()
     if target_file.content_type == 'application/pdf':
         images = convert_from_bytes(request_file,500,poppler_path="C:\Program Files\poppler-22.12.0\Library\\bin")
@@ -60,6 +98,7 @@ def text(request,pk):
         textObj = TextModel.objects.filter(pk=pk,user=request.user).first()
         if textObj is None:
             return HttpResponse('Access Denied', status=403)
+
         context = {}
         context['singleTextData'] = textObj.text
         return render(request,'text.html',context)
@@ -67,47 +106,7 @@ def text(request,pk):
     else:
         return HttpResponse('Unauthorized', status=401)
 
-def login(request):
-    if request.method == "POST":
-        data = request.POST
-        username = data['username']
-        password = data['password']
 
-        user = authenticate(request,username=username,password=password)
-        if user is not None:
-            auth_login(request,user)
-            return redirect("home")
-        else:
-            messages.error(request,"Invalid Credentials")
-            return redirect('login')
-    return render(request,"login.html")
-
-
-def signup(request):
-    if request.method == "POST":
-        data = request.POST
-        name = data['name']
-        username = data['username']
-        password = data['password']
-        confirm_password = data['confirm_password']
-
-        user = User.objects.filter(username=username).exists()
-        if not user :
-            if password == confirm_password:
-                user = User.objects.create_user(username=username,password=password,first_name=name)
-                return redirect("login")
-            else:
-                messages.error(request,"Password does not match")
-                return redirect('signup')
-        else:
-            messages.error(request, "Username already exist")
-            return redirect('signup')
-
-    return render(request,"signup.html")
-
-def logout(request):
-    auth_logout(request)
-    return redirect("login")
 
         
     
